787c9989e7f5cc8641047dc1b0e0c239232fa40d
roz3x <meeshivang@gmail.com>
2020-03-09 00:58:28 +0530

    chaged few issues

--- index.js
+++ index.js
@@ -12,38 +12,72 @@ window.onload = () => {
 
     // now the fetch thingy
 
-    display("nature")
+    display("orange")
 
     document.getElementById('search_button').onclick = () => {
         display(document.getElementById('search').value)
     }
+
+   
 }
 
-function display ( query ) {
-    fetch("https://api.unsplash.com/search/photos?query="+query+"&count=20&client_id="+client_id)
-    .then(data => data.json())
-    .then(data => {
-        console.log(data)
+function display(query) {
+    fetch("https://api.unsplash.com/search/photos?query=" + query + "&count=20&client_id=" + client_id)
+        .then(data => data.json())
+        .then(data => {
+            console.log(data)
 
-        let parent = document.getElementById('photos')
-        remove_childs(parent)
+            let parent = document.getElementById('photos')
+            remove_childs(parent)
 
-        for (let i=0;i<data.results.length;i++){
-            let photo = document.createElement('img')
-            photo.src = data.results[i].urls.regular
-            parent.appendChild(photo)
-        }
-    })
-    
-    //catching the error and displaying #IMPORTANT for phony browsers
-    .catch(err => {
-        document.getElementById('error').innerHTML = "oops an error occured namely :"+err
-        console.log(err)
-    })
+            for (let i = 0; i < data.results.length; i++) {
+                let div = document.createElement('div')
+                let photo = new Image()
+                photo.onload = () => {
+                    // photo.style.display = "block"
+
+                    photo.animate([
+                        {opacity: '0'},
+                        {opacity: '1'}
+                    ], {
+                        duration: 2000,
+                        iterations: 1,
+                    })
+                    photo.style.opacity  = '1'
+                }
+                photo.style.opacity = '0'
+                console.log(data.results[i].height)
+
+                // giving the outer div the height that is required
+
+                div.style.height = (data.results[i].height / (data.results[i].width/document.body.clientWidth)).toString() + "px"
+                div.style.maxWidth = document.body.clientWidth
+                div.style.background = data.results[i].color
+                console.log( div.style.color)
+
+                photo.style.height = '100%'
+                photo.style.width = '100%'
+
+
+
+                photo.src = data.results[i].urls.regular
+
+                div.appendChild(photo)
+                parent.appendChild(div) 
+            }
+        })
+
+        //catching the error and displaying #IMPORTANT for phony browsers
+        .catch(err => {
+            document.getElementById('error').innerHTML = "oops an error occured namely :" + err
+            console.log(err)
+        })
 }
+
+
 function remove_childs(node) {
     let last_child = node.lastChild
-    while(last_child!= null){
+    while (last_child != null) {
         node.removeChild(last_child)
         last_child = node.lastChild
     }
